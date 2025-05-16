a05c3a7843f9572a4cfed93246ff1298e5c4aa7d
AI-LORD <ogbhuwanbhatt@gmail.com>
2025-05-16 22:07:36 +0545

    Add files via upload

---
+++ www.py
@@ -0,0 +1,14 @@
+
+Name = "Bhuwan Bhatt"
+age = 99
+
+print(Name)
+
+print("age 99")
+# add two numbers
+First_number = input("Enter the first number\n")
+Second_number = input("Enter the second number\n")
+
+sum = float(First_number) +int(Second_number)
+
+print(sum)
